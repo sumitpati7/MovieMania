53a20077ba728df59fd2fe6515539d4e67d1445c
Sumit Pati <sumitpati7@gmail.com>
2025-07-13 10:03:49 +0545

    Fix movie list

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies, selectMovies } from "../redux/movieSlice";
+
 const options = {
   method: "GET",
   headers: {
@@ -9,57 +10,43 @@ const options = {
   },
 };
 
-const MovieList = () => {
-  const filter_type = [
-    {
-      title: "Now Playing",
-      url_snip: "now_playing",
-    },
-    {
-      title: "Popular",
-      url_snip: "popular",
-    },
-    {
-      title: "Top Rated",
-      url_snip: "top_rated",
-    },
-    {
-      title: "Upcoming",
-      url_snip: "upcoming",
-    },
-  ];
+const filter_type = [
+  { title: "Now Playing", url_snip: "now_playing" },
+  { title: "Popular", url_snip: "popular" },
+  { title: "Top Rated", url_snip: "top_rated" },
+  { title: "Upcoming", url_snip: "upcoming" },
+];
 
+const MovieList = () => {
   const [filter, setFilter] = useState("now_playing");
   const dispatch = useDispatch();
   const movies = useSelector(selectMovies);
-
-  useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=" +
-        process.env.REACT_APP_API_KEY,
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => dispatch(fetchMovies(response.results)))
-      .catch((err) => console.error(err));
-  }, [dispatch]);
+  const apiKey = process.env.REACT_APP_API_KEY;
 
   const fetchData = useCallback(
-    (filter) => {
-      setFilter(filter);
-      fetch(
-        "https://api.themoviedb.org/3/movie/" +
-          filter +
-          "?language=en-US&page=1&api_key=" +
-          process.env.REACT_APP_API_KEY,
-        options
-      )
-        .then((response) => response.json())
-        .then((response) => dispatch(fetchMovies(response.results)))
-        .catch((err) => console.error(err));
+    async (category) => {
+      if (!apiKey) {
+        console.error("API key is missing. Check your .env file.");
+        return;
+      }
+
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1&api_key=${apiKey}`,
+          options,
+        );
+        const data = await res.json();
+        dispatch(fetchMovies(data.results || []));
+      } catch (err) {
+        console.error("Error fetching movies:", err);
+      }
     },
-    [dispatch]
+    [dispatch, apiKey],
   );
+
+  useEffect(() => {
+    fetchData(filter);
+  }, [filter, fetchData]);
 
   return (
     <div className="w-full py-8">
@@ -68,9 +55,7 @@ const MovieList = () => {
           name="filter"
           id="filter"
           value={filter}
-          onChange={(e) => {
-            fetchData(e.target.value);
-          }}
+          onChange={(e) => setFilter(e.target.value)}
         >
           {filter_type.map((value, index) => (
             <option key={index} value={value.url_snip}>
@@ -79,9 +64,10 @@ const MovieList = () => {
           ))}
         </select>
       </div>
+
       <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-        {movies.map((value, index) => (
-          <MovieCard className="mx-auto" key={index} movie={value} />
+        {movies.map((movie, index) => (
+          <MovieCard className="mx-auto" key={index} movie={movie} />
         ))}
       </div>
     </div>
