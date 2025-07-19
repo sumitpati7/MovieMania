ef1d9892e79c5036dacec6a17556263fdd2952b7
Sumit Pati <sumitpati7@gmail.com>
2025-07-19 18:21:06 +0545

    Update to host to github

--- src/index.js
+++ src/index.js
@@ -3,41 +3,28 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./Components/Homepage";
 import MovieDetail from "./Components/MovieDetail";
 import Navbar from "./Components/navbar";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Homepage />,
-      },
-      {
-        path: "details/:movieId",
-        element: <MovieDetail />,
-      },
-    ],
-  },
-]);
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Navbar />
-      <RouterProvider router={routes} />
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Homepage />} />
+            <Route path="details/:movieId" element={<MovieDetail />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
